ee78ff39f6871cbcb1d2c14b0613fa065ae7f9e3
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-07-29 01:30:15 +0300

    Initial Commit

---
+++ WordsGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordsGame
+//
+//  Created by ALEKSEY SUSLOV on 29.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
